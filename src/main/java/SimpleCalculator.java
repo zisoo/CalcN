1eaf0780d115ef3098c86b9f23281b3f344fc1e6
namyun kim <nykim@hansung.ac.kr>
2016-04-11 17:49:34 +0900

    어리석은 수정

--- src/main/java/SimpleCalculator.java
+++ src/main/java/SimpleCalculator.java
@@ -17,10 +17,9 @@ public class SimpleCalculator {
 	}
 
 	public void div(int x, int y) {
-		if (y!=0)
+		
 			result = x/y;
-		else 
-			System.out.println("분모가 0입니다");
+	
 
 	}
 	public int getResult() {
