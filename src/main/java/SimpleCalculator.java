ef1234a539898a34d17b0cae366dafe5f05a5af3
namyun kim <nykim@hansung.ac.kr>
2016-04-11 16:28:11 +0900

    add 기능/테스트 코드 추가
    계산기의 가장 기본적 기능

---
+++ src/main/java/SimpleCalculator.java
@@ -0,0 +1,14 @@
+
+
+public class SimpleCalculator {
+	private int result=0;
+	
+	public void add(int x, int y) {
+		result = x+y;
+	}
+	
+	public int getResult() {
+		return result;
+	}
+
+}
---
+++ src/test/java/SimpleCalculatorTest.java
@@ -0,0 +1,17 @@
+
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class SimpleCalculatorTest {
+
+	@Test
+	public void testAdd() {
+		SimpleCalculator calc = new SimpleCalculator();
+		calc.add(10, 20);
+		assertEquals(30, calc.getResult());
+		
+	}
+
+}
