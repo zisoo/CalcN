27f63d726b65e63c0553fe6b9770551bd2ce2ca7
namyun kim <nykim@hansung.ac.kr>
2016-04-11 17:37:26 +0900

    나눗셈 오류 수정

--- src/main/java/SimpleCalculator.java
+++ src/main/java/SimpleCalculator.java
@@ -17,7 +17,10 @@ public class SimpleCalculator {
 	}
 
 	public void div(int x, int y) {
-		result = x/y;
+		if (y!=0)
+			result = x/y;
+		else 
+			System.out.println("분모가 0입니다");
 
 	}
 	public int getResult() {
